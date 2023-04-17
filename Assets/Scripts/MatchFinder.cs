190d438f15cb4c7ab34ee6cbecd100bd94a3f246
ALP TUNA <alp.tuna.453@gmail.com>
2023-04-17 21:00:18 +0300

    Correct row match condition

--- Assets/Scripts/MatchFinder.cs
+++ Assets/Scripts/MatchFinder.cs
@@ -17,24 +17,26 @@ public class MatchFinder : MonoBehaviour
 
     public void findAllMatches(){
         for(int i=0;i<board.height;i++){
+            Gem.GemType candidateGemType = board.allGems[0, i].type;
+            bool isRowMatch = true; 
+            for(int j=1;j<board.width;j++){
 
-            for(int j=0;j<board.width - 4;j++){
-                if(board.allGems[j, i] != null && board.allGems[j + 1, i] != null && board.allGems[j + 2, i] != null && board.allGems[j + 3, i] != null && board.allGems[j + 4, i]){
-                    if(board.allGems[j, i].type == board.allGems[j + 1, i].type && board.allGems[j, i].type == board.allGems[j + 2, i].type && board.allGems[j, i].type == board.allGems[j + 3, i].type && board.allGems[j, i].type == board.allGems[j + 4, i].type){
-                        board.allGems[j, i].isMatched = true;
-                        board.allGems[j + 1, i].isMatched = true;
-                        board.allGems[j + 2, i].isMatched = true;
-                        board.allGems[j + 3, i].isMatched = true;
-                        board.allGems[j + 4, i].isMatched = true;
-                        currentMatches.Add(board.allGems[j, i]);
-                        currentMatches.Add(board.allGems[j + 1, i]);
-                        currentMatches.Add(board.allGems[j + 2, i]);
-                        currentMatches.Add(board.allGems[j + 3, i]);
-                        currentMatches.Add(board.allGems[j + 4, i]);
-                    }
+                if(board.allGems[j,i].type != candidateGemType){
+                    isRowMatch = false;
+                    break;
                 }
 
             }
+
+            if(isRowMatch){
+
+                for(int j=0;j<board.width;j++){
+                    board.allGems[j, i].isMatched = true;
+                    currentMatches.Add(board.allGems[j, i]);
+                }
+
+            }
+
         }
     }
 }
